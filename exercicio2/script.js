4b6dd4f4b4f257c4a25169a394a3a433eb61f9c4
thiagogagliari <thiagogagliari@hotmail.com>
2022-08-09 18:05:20 -0300

    barbosa-thiago-gagliari

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,24 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+// UTILIZANDO SWITCH CASE
+
+const country = (prompt('Qual sua nacionalidade:')).toLowerCase();
+
+switch(country){
+    case 'brasileira': 
+        console.log('A pessoa é brasileira')
+        break;
+    case 'argentina':
+        console.log('A pessoa é argentina')
+        break;
+    case 'uruguaia':
+        console.log('A pessoa é Uruguaia')
+        break;
+    case 'chilena':
+        console.log('A pessoa é chilena')
+        break;
+    case 'colombiana':
+        console.log('A pessoa é colombiana')
+        break;
+    default:
+        console.log('Nacionalidade não encontrada');
 }
